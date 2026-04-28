ace953f432dc11aa33b68824b88e610296073b6f
mehmet turac <mehmetturac@gmail.com>
2026-04-28 11:34:45 +0300

    🚀 Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# 🚀 REPO GHOST
+
+> CLI tool by TURAC
+
+```bash
+npm install -g repo-ghost
+repo-ghost
+```
+
+---
+
+<div align="center">
+  <sub>Built with 💜 by <strong>TURAC</strong></sub>
+</div>
---
+++ bin/index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+const chalk = require('chalk');
+console.log('\n' + chalk.cyan('🚀 REPO GHOST'));
+console.log(chalk.yellow('  Work in progress...'));
+console.log(chalk.gray('  Built by TURAC\n'));
